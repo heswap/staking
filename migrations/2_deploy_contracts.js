f3ae3b6871855f33a8472fa6095b44cbae519977
heswap <heswap@protonmail.com>
2021-06-23 14:24:55 +0800

    modify heco to bsc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const MastefChef = artifacts.require('MasterChef');
 module.exports = async function(deployer) {
 
   await deployer.deploy(MastefChef,
-    "0xC17FA8E2310f1920Ce592b267Bb16d5f00336155",
+    "0x10658Fc030f90CFf7608b21eC522A29645D515a3",
     "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
     "0x5dE4C680d3e306eBbd94b5795905f8234B22D803",
     "30000000000000000000",
@@ -13,6 +13,5 @@ module.exports = async function(deployer) {
     '10000'
   );
 
-  const instanceMasterChef = await MastefChef.deployed();
 };
 
